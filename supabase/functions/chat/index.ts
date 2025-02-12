056aac6c5ef0a5cac1a5c4d7a4b4b0303c6439d1
yayeet999 <serenityai904@gmail.com>
2025-02-12 16:36:25 -0600

    Update index.ts

--- supabase/functions/chat/index.ts
+++ supabase/functions/chat/index.ts
@@ -282,7 +282,7 @@ Use these details as your personal background as your identity and reveal them n
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        model: "ft:gpt-4o-mini-2024-07-18:practice:comb1-27:AuEcwhks",
+        model: "ft:gpt-4o-mini-2024-07-18:practice:noemoticons:AzwRKS4t",
         temperature: 0.7,
         frequency_penalty: 0.3,
         presence_penalty: 0.3,
