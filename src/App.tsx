6e2c8398b9d473413eb68e9adc949beb529c1d1b
yayeet999 <serenityai904@gmail.com>
2025-02-20 02:22:13 -0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { AuthProvider } from "./components/AuthProvider";
-import { ProtectedRoute } from "./components/ProtectedRoute";
+import { AuthProvider } from "@/components/AuthProvider";
+import { ProtectedRoute } from "@/components/ProtectedRoute";
 
-import LandingPage from "./components/LandingPage";
-import ChatInterface from "./components/ChatInterface";
-import NavigationSidebar from "./components/NavigationSidebar";
-import Dashboard from "./components/Dashboard";
-import Settings from "./components/Settings";
-import Support from "./components/Support";
+import Landing from "@/components/LandingPage";
+import ChatInterface from "@/components/ChatInterface";
+import NavigationSidebar from "@/components/NavigationSidebar";
+import Dashboard from "@/components/Dashboard";
+import Settings from "@/components/Settings";
+import Support from "@/components/Support";
 import Auth from "./pages/Auth";
 import Onboarding from "./components/Onboarding";
 import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfService from "./pages/TermsOfService";
+import ProfileCarousel from "@/components/Profiles";
 
 const queryClient = new QueryClient();
 
@@ -31,7 +31,7 @@ const App = () => {
           <AuthProvider>
             <div className="min-h-screen bg-softgray">
               <Routes>
-                <Route path="/" element={<LandingPage />} />
+                <Route path="/" element={<Landing />} />
                 <Route path="/auth" element={<Auth />} />
                 <Route path="/onboarding" element={<Onboarding />} />
                 <Route path="/privacy-policy" element={<PrivacyPolicy />} />
@@ -42,6 +42,15 @@ const App = () => {
                     <ProtectedRoute>
                       <NavigationSidebar />
                       <Dashboard />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path="/profiles"
+                  element={
+                    <ProtectedRoute>
+                      <NavigationSidebar />
+                      <ProfileCarousel />
                     </ProtectedRoute>
                   }
                 />
